a8e0e6f970c1673ca2d08acf3e991bd97bd2e6d6
Virgil Chen <virgilchen97@gmail.com>
2020-09-20 02:05:52 -0500

    2020-09-20

--- 0150.逆波兰表达式求值/0150-逆波兰表达式求值.java
+++ 0150.逆波兰表达式求值/0150-逆波兰表达式求值.java
@@ -3,21 +3,25 @@ import java.util.*;
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
-        for(String now:tokens){
-            if(now.equals("+") || now.equals("-") || now.equals("*") || now.equals("/")){
+        for(String token:tokens){
+            if("*".equals(token)){
                 int b = stack.pop();
                 int a = stack.pop();
-                if(now.equals("+")){
-                    stack.push(a + b);
-                }else if(now.equals("-")){
-                    stack.push(a - b);
-                }else if(now.equals("*")){
-                    stack.push(a * b);
-                }else{
-                    stack.push(a / b);
-                }
+                stack.push(a*b);
+            }else if("/".equals(token)){
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a/b);
+            }else if("+".equals(token)){
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a+b);
+            }else if("-".equals(token)){
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a-b);
             }else{
-                stack.push(Integer.parseInt(now));
+                stack.push(Integer.parseInt(token));
             }
         }
         return stack.pop();
