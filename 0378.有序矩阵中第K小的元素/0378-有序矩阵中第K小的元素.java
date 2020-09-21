f40f403053a97de70d2a55a44559e67f40a2e34b
Virgil Chen <virgilchen97@gmail.com>
2020-09-20 23:15:28 -0500

    2020-09-20

--- 0378.有序矩阵中第K小的元素/0378-有序矩阵中第K小的元素.java
+++ 0378.有序矩阵中第K小的元素/0378-有序矩阵中第K小的元素.java
@@ -1,25 +1,38 @@
 import java.util.*;
 
 class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(k, new Comparator<Integer>() {
-            public int compare(Integer a, Integer b) {
-                return b - a;
-            }
-        });
+    class Pair implements Comparable<Pair> {
+        int val;
+        int from;
 
-        for (int[] row : matrix) {
-            for (int num : row) {
-                if (queue.size() < k) {
-                    queue.offer(num);
-                } else {
-                    if (queue.peek() > num) {
-                        queue.poll();
-                        queue.offer(num);
-                    }
-                }
-            }
+        public Pair(int val, int from) {
+            this.val = val;
+            this.from = from;
         }
-        return queue.peek();
+
+        @Override
+        public int compareTo(Solution.Pair arg0) {
+            return this.val - arg0.val;
+        }
+    }
+
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Pair> q = new PriorityQueue<>(matrix.length);
+        int[] ptrs = new int[matrix.length];
+
+        for (int i = 0; i < ptrs.length; i++) {
+            q.add(new Pair(matrix[i][0], i));
+            ptrs[i]++;
+        }
+
+        while (k > 1) {
+            Pair now = q.poll();
+            if (ptrs[now.from] < matrix[0].length) {
+                q.add(new Pair(matrix[now.from][ptrs[now.from]++], now.from));
+            }
+            k--;
+        }
+
+        return q.poll().val;
     }
 }
